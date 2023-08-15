1e1c2bf4bd3e159c14e97425b0623d3dde1b3a23
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-08-15 17:14:57 +0200

    Completed Examplebot

---
+++ Commands/MyKawaiiCommands.cs
@@ -0,0 +1,48 @@
+﻿using DisCatSharp.ApplicationCommands;
+using DisCatSharp.ApplicationCommands.Attributes;
+using DisCatSharp.ApplicationCommands.Context;
+using DisCatSharp.Entities;
+using DisCatSharp.Enums;
+using KawaiiAPI.NET;
+using KawaiiAPI.NET.Enums;
+
+
+namespace ExampleBot.Commands;
+
+public class MyKawaiiCommands : ApplicationCommandsModule
+{
+    private readonly KawaiiClient _kawaiiClient;
+
+    public MyKawaiiCommands(KawaiiClient kawaiiClient)
+    {
+        _kawaiiClient = kawaiiClient;
+    }
+
+    [SlashCommand("hug", "Hugs someone")]
+    public async Task HugCommand(InteractionContext ctx, [Option("user", "The user to hug")] DiscordUser user)
+    {
+        // Get a random hug gif from api and save it to imageUrl
+        var imageUrl = await _kawaiiClient.GetRandomGifAsync(KawaiiGifType.Hug);
+
+        // Create a new embed with the image url
+        var embed = new DiscordEmbedBuilder()
+            .WithImageUrl(imageUrl)
+            .WithTitle($"{ctx.Member.DisplayName} hugs {user.Username}!")
+            .WithColor(DiscordColor.Blurple)
+            .WithFooter("Powered by KawaiiAPI.NET");
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(embed));
+    }
+
+    [SlashCommand("kiss", "Kisses someone")]
+    public async Task KissCommand(InteractionContext ctx, [Option("user", "The user to kiss")] DiscordUser user)
+    {
+        var imageUrl = await _kawaiiClient.GetRandomGifAsync(KawaiiGifType.Kiss);
+        var embed = new DiscordEmbedBuilder()
+            .WithImageUrl(imageUrl)
+            .WithTitle($"{ctx.Member.DisplayName} kisses {user.Username}!")
+            .WithColor(DiscordColor.Blurple)
+            .WithFooter("Powered by KawaiiAPI.NET");
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(embed));
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,60 @@
-﻿
+﻿using DisCatSharp;
+using DisCatSharp.ApplicationCommands;
+using DisCatSharp.Enums;
+using KawaiiAPI.NET;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace ExampleBot;
+
+internal class Bot : ApplicationCommandsModule
+{
+    private static void Main(string[] args)
+    {
+        RunBotAsync().GetAwaiter().GetResult();
+    }
+
+    private static async Task RunBotAsync()
+    {
+        string Token = "Discord-Bot-Token"; // Never save your token in your code, use a config file or environment variables instead. This is just an example.
+
+        // Create a new service collection
+        ServiceCollection services = new();
+
+
+        // Register KawaiiClient in the service collection
+        services.AddSingleton<KawaiiClient>(sp =>
+        {
+            var apiKey = "your-token-here"; // Replace with your actual API key // See https://kawaii.red/dashboard
+            return new KawaiiClient(apiKey);
+        });
+
+        // Build the service provider
+        ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+        // Create a new Discord client
+        DiscordClient client = new(new DiscordConfiguration
+        {
+            Token = Token,
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.All,
+            AutoReconnect = true,
+            ServiceProvider = serviceProvider,
+            MinimumLogLevel = LogLevel.Debug,
+        });
+
+        // Register the commands
+        var appCommands = client.UseApplicationCommands(new ApplicationCommandsConfiguration
+        {
+            ServiceProvider = serviceProvider,
+        });
+        appCommands.RegisterGlobalCommands(Assembly.GetExecutingAssembly());
+
+        // Connect to Discord
+        await client.ConnectAsync();
+
+        // Block this task until the program is closed. Prevents the bot from exiting.
+        await Task.Delay(-1);
+    }
+}
